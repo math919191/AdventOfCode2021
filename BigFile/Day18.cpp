3e71a53effb43572eb8ec2b99f6993a55b1c5ed5
math919191 <math919191@gmail.com>
2022-05-02 23:25:00 -0600

    working

--- BigFile/Day18.cpp
+++ BigFile/Day18.cpp
@@ -1,7 +1,6 @@
 //
 // Created by rebek on 4/30/2022.
 //
-#define MAGIC_NUM -1;
 
 #include "Day18.h"
 
@@ -22,7 +21,7 @@ int Day18::findIfInside4Pairs(string snailFishNum){
             return i;
         }
     }
-    return MAGIC_NUM;
+    return -1;
 }
 
 int Day18::findIfNeedsSplitting(string snailFishNum){
@@ -40,81 +39,149 @@ int Day18::findIfNeedsSplitting(string snailFishNum){
 
 string Day18::split(string snailNum){
     int index = findIfNeedsSplitting(snailNum);
-    string newPair = "[,]";
-    int num = stoi(snailNum.substr(index, 2));
-    if (num % 2 == 1){
-        newPair.insert(newPair.begin()+1, num/2 + 48);
-        newPair.insert(newPair.begin()+3, num/2 + 49);
-    } else {
-        newPair.insert(newPair.begin()+1, num/2 + 48);
-        newPair.insert(newPair.begin()+3, num/2 + 48);
+    string newPair = "[";
+
+    int commaIndex = 0;
+    bool actualIndex = true;
+    while (true){
+        if (snailNum.at(index+commaIndex) == ',') {
+            commaIndex = commaIndex+index;
+            break;
+        }
+        else {
+            if (snailNum.at(index+commaIndex) == ']') {
+                actualIndex = false;
+                break; // it's on the other side
+            }
+            commaIndex++;
+        }
     }
-    snailNum.replace(snailNum.begin()+index, snailNum.begin()+index+2,newPair);
+    if (actualIndex == false){
+
+        commaIndex = 0;
+        while (true){
+            if (snailNum.at(index-commaIndex) == ',') {
+                commaIndex = index-commaIndex;
+                break;
+            }
+            else {
+                commaIndex++;
+            }
+        }
+    }
+
+
+
+    int num = stoi(snailNum.substr(index, commaIndex-index));
+    int half = num/2;
+    string halfStr = intToStr(half);
+    string halfStrPlus1 = intToStr(half+1);
+    if (num % 2 == 1){
+        newPair += halfStr;
+        newPair += ",";
+        newPair += halfStrPlus1;
+        newPair += "]";
+    } else {
+        newPair += halfStr;
+        newPair += ",";
+        newPair += halfStr;
+        newPair += "]";
+    }
+    string currCurrNum = snailNum;
+    string item = snailNum.substr(index, commaIndex);
+    if (index < commaIndex) snailNum.replace(snailNum.begin()+index, snailNum.begin()+commaIndex,newPair);
+    else snailNum.replace(snailNum.begin()+commaIndex+1, snailNum.begin()+index+2,newPair);
+    cout << currCurrNum << endl;
+    cout << snailNum << endl;
     return snailNum;
 }
 
 string Day18::findNextLeftNumber(int index, string snailNum){
+    //index is the left bracket
     int leftNumIndex = -1;
+    int leftNumBegIndex = -1;
     for (int i = index; i >= 0; i--){
         if (isdigit(snailNum.at(i))){
             leftNumIndex = i;
+            while (true){
+                if (isdigit(snailNum.at(i))){
+                    i--;
+                } else {
+                    leftNumBegIndex = i+1;
+                    break;
+                }
+            }
             break;
         }
     }
 
-    if (leftNumIndex > 0){
+    if (leftNumIndex > 0) {
+        int commaIndex = 0;
+        for (int i = 0; i < 20; i++){
+            if (snailNum.at(index+i) == ','){
+                commaIndex = index+i;
+                break;
+            }
+        }
 
-        int num1 = snailNum.at(leftNumIndex ) - 48;
-        int num2 = snailNum.at(index+1) - 48;
+
+        int num1 = stoi(snailNum.substr(leftNumBegIndex, leftNumIndex-leftNumBegIndex+1));
+        int num2 = stoi(snailNum.substr(index+1, commaIndex-index-1));
         int sum =  num1 + num2;
-        stringstream ss;
-        ss << sum;
-        string strSum = ss.str();
-        snailNum.replace(snailNum.begin()+leftNumIndex, snailNum.begin()+leftNumIndex+1,strSum);
-
-        //if (isdigit((leftNumIndex-1))){}
+        string strSum = intToStr(sum);
+        snailNum.replace(snailNum.begin()+leftNumBegIndex, snailNum.begin()+leftNumIndex+1,strSum);
     }
-//    else {
-//        snailNum.at(index+1) = '0';
-//        if (isdigit(snailNum.at(index+2))){
-//            snailNum.erase(snailNum.begin() + index+2);
-//        }
-//    }
+
     return snailNum;
 }
 
 string Day18::findNextRightNumber(int index, string snailNum){
     int rightNumIndex = -1;
-    index = findIfInside4Pairs(snailNum)+4;
+    index = findIfInside4Pairs(snailNum);
+    //adjusting the index if they moved because of numbers
+    int i = 0;
+    while (true){
+        if (snailNum.at(index+i) == ']') break;
+        else i++;
+    }
+    index += i;
+    //index is the right bracket
+    int rightNumEndIndex = 0;
+
     for (int i = index; i < snailNum.length(); i++){
         char item = snailNum.at(i);
         if (isdigit(snailNum.at(i))){
             rightNumIndex = i;
+            while (true){
+                if (isdigit(snailNum.at(i))){
+                    i++;
+                } else {
+                    rightNumEndIndex = i-1;
+                    break;
+                }
+            }
             break;
         }
     }
 
-    if (rightNumIndex  > 0){
-//        char a = '4';
-//        int ia = a - '0';
-//        char item = snailNum.at(rightNumIndex);
+    if (rightNumIndex  > 0){ // if it is negative one, there is no number to the right
+
+        int commaIndex = 0;
+        for (int i = 0; i < 20; i++){
+            if (snailNum.at(index-i) == ','){
+                commaIndex = index-i;
+                break;
+            }
+        }
+
+        int num1 = stoi(snailNum.substr(rightNumIndex, rightNumEndIndex-rightNumIndex+1));
+
+        int num2 = stoi(snailNum.substr(commaIndex+1, index-commaIndex-1));
 //        int num1 = snailNum.at(rightNumIndex ) - 48;
 //        int num2 = snailNum.at(index-1) - 48;
-//        int sum =  num1+num2 +48;
-//        snailNum.at(rightNumIndex) = sum;
-//        //if (isdigit((leftNumIndex-1))){}
-        int num1 = snailNum.at(rightNumIndex ) - 48;
-        int num2 = snailNum.at(index-1) - 48;
         int sum =  num1 + num2;
-        stringstream ss;
-        ss << sum;
-        string strSum = ss.str();
-
-
-
-        snailNum.replace(snailNum.begin()+rightNumIndex, snailNum.begin()+rightNumIndex+1,strSum);
-
-
+        string strSum = intToStr(sum);
+        snailNum.replace(snailNum.begin()+rightNumIndex, snailNum.begin()+rightNumEndIndex+1,strSum);
 
     }
     return snailNum;
@@ -123,10 +190,16 @@ string Day18::findNextRightNumber(int index, string snailNum){
 string Day18::explode(int index, string snailNum){
     snailNum = findNextLeftNumber(index, snailNum);
     //cout << "after left "<< snailNum << endl;
-    snailNum = findNextRightNumber(index+4, snailNum);
+
+    snailNum = findNextRightNumber(index, snailNum);
     //cout << "after right "<< snailNum << endl;
     index = findIfInside4Pairs(snailNum);
-    snailNum.replace(snailNum.begin()+index, snailNum.begin()+index+5,"0");
+    int distToBracket = 0;
+    while (true){
+        if (snailNum.at(index+distToBracket) == ']') break;
+        else distToBracket++;
+    }
+    snailNum.replace(snailNum.begin()+index, snailNum.begin()+index+distToBracket+1,"0");
     //cout << "after replace "<< snailNum << endl;
     return snailNum;
 }
@@ -174,39 +247,34 @@ int Day18::findMagnitude(string snailNum){
 }
 
 void Day18::test(){
-
     cout << findMagnitude("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]") << endl;
 }
 
 int Day18::solve(){
 
-    //string currTest = "[[[[[9,8],1],2],3],4]";
-    //string currTest = "[7,[6,[5,[4,[3,2]]]]]";
-    //string currTest = "[[6,[5,[4,[3,2]]]],1]";
     //string currTest = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]";
-    //string currTest = "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]";
-    string currTest = "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]";
-//    int index = findIfInside4Pairs(currTest);
-//    cout << explode(index, currTest) << endl;
+    //cout << "exploding: " << explode(findIfInside4Pairs(currTest), currTest);
+
+//#if 0
     string currNum = myInput.at(0);
     for (int i = 0; i < myInput.size(); i++){
-        cout << currNum << endl;
+        cout << currNum << endl << endl;
         if (i != myInput.size()-1){
             currNum = add(currNum, myInput.at(i+1));
         }
-        //cout << "after addition: " << currNum << endl;
+
         while (true){
             while (true){
                 if (findIfInside4Pairs(currNum) > 0){
                     currNum = explode(findIfInside4Pairs(currNum), currNum);
-                //    cout << "after explode:  "<< currNum << endl;
+                    cout << "after explode " << currNum << endl << endl;
                 } else {
                     break;
                 }
             }
             if (findIfNeedsSplitting(currNum) > 0){
                 currNum = split(currNum);
-            //    cout << "after split:    "<< currNum << endl;
+                cout << "after split " << currNum << endl << endl;
             }
             if (findIfNeedsSplitting(currNum) < 0 && findIfInside4Pairs(currNum) < 0){
                 break;
@@ -214,7 +282,9 @@ int Day18::solve(){
         }
     }
     cout << "final answer: " << currNum << endl;
-
+    cout << "final answer: " << findMagnitude(currNum) << endl;
+//#endif
     cout << endl << endl;
+
     return 18;
 }
